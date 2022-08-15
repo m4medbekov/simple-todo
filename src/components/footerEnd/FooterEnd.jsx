439b1d5cf97b5f86a4ac4a8151ff46ac93033735
Ruslan Mamedbekov <m4medbekov@gmail.com>
2022-08-15 17:13:08 +0300

    Fix desktop position

--- src/components/footerEnd/FooterEnd.jsx
+++ src/components/footerEnd/FooterEnd.jsx
@@ -2,9 +2,9 @@ import { Layout, Typography } from 'antd';
 const { Title } = Typography;
 const { Footer } = Layout;
 
-const FooterEnd = () => {
+const FooterEnd = ({ windowWith }) => {
   return (
-    <Footer style={{ position: "fixed", bottom: 0, width: "100%", padding: "5px 50px" }}>
+    <Footer style={{ position: windowWith <= 1024 ? "fixed" : "relative", bottom: 0, width: "100%", padding: "5px 50px" }}>
       <Title style={{ margin: 0 }} level={5}>© MRA 2022</Title>
     </Footer>
   )
