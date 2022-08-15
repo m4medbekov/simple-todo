bc66c90f815a2f3df432def3ce48228bc4e04797
Ruslan Mamedbekov <m4medbekov@gmail.com>
2022-08-15 17:06:25 +0300

    Again footer fix

--- src/components/footerEnd/FooterEnd.jsx
+++ src/components/footerEnd/FooterEnd.jsx
@@ -4,7 +4,7 @@ const { Footer } = Layout;
 
 const FooterEnd = () => {
   return (
-    <Footer style={{ position: "absolute", bottom: 0, width: "100%", padding: "5px 50px" }}>
+    <Footer style={{ position: "fixed", bottom: 0, width: "100%", padding: "5px 50px" }}>
       <Title style={{ margin: 0 }} level={5}>© MRA 2022</Title>
     </Footer>
   )
