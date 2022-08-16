0b5ec9f43a61145e78b9276fb5f05725be291ddc
Ruslan Mamedbekov <m4medbekov@gmail.com>
2022-08-16 14:24:57 +0300

    Add test redux state

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,18 +1,24 @@
-import { Layout } from 'antd';
+import { Button, Layout } from 'antd';
 import { useState } from 'react';
 import ContentBody from './contentBody/ContentBody';
 import FooterEnd from './footerEnd/FooterEnd';
 import HeaderTitle from './header/Header';
-import Sidebar from './siderbar/Sidebar';
+import Sidebar from './siderbar/Sidebar'; 
+import { useSelector, useDispatch } from 'react-redux/es/exports';
+import { decrementCounter, incrementCounter } from '../store/counterSlice';
 
 const MainContainer = () => {
+  
+  const counters = useSelector(state => state.counter);
+  const counter = counters.counter;
+  const dispatch = useDispatch();
 
   const [windowWith, setWindowWidth] = useState(window.outerWidth);
 
   const todoList = [
     { id: 0, checked: false, text: "Доделать проект" },
     { id: 1, checked: true, text: "Посмотреть Хэллбой 2019" }
-  ]
+  ];
 
   window.addEventListener("resize", (event) => {
     setWindowWidth(event.currentTarget.outerWidth);
@@ -20,6 +26,9 @@ const MainContainer = () => {
 
   return (
     <>
+      <div>{counter}</div>
+      <Button onClick={() => dispatch(incrementCounter())}>Inc</Button>
+      <Button onClick={() => dispatch(decrementCounter())}>Dec</Button>
       <Layout style={{ 
           width: windowWith <= 1024 ? "100%" : "50%", 
           margin: "auto", 
