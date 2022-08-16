db8c5539225f3929f3004b0a1068fde0969d5439
Ruslan Mamedbekov <m4medbekov@gmail.com>
2022-08-16 12:27:11 +0300

    Test full screen button

--- src/App.js
+++ src/App.js
@@ -1,46 +1,20 @@
 import './App.css';
 import 'antd/dist/antd.min.css';
 import MainContainer from './components/MainContainer';
+import { Button } from 'antd';
+import { FullScreen, useFullScreenHandle } from "react-full-screen";
 
 
 function App() {
 
-  (function() {
-    
-    var win = window,
-        doc = win.document;
+  const handle = useFullScreenHandle();
   
-    // If there's a hash, or addEventListener is undefined, stop here
-    if ( !window.location.hash || !win.addEventListener ) {
-  
-      //scroll to 1
-      window.scrollTo( 0, 1 );
-      var scrollTop = 1,
-  
-      //reset to 0 on bodyready, if needed
-      bodycheck = setInterval(function(){
-        if( doc.body ){
-          clearInterval( bodycheck );
-          scrollTop = "scrollTop" in doc.body ? doc.body.scrollTop : 1;
-          win.scrollTo( 0, scrollTop === 1 ? 0 : 1 );
-        } 
-      }, 15 );
-  
-      if (win.addEventListener) {
-        win.addEventListener("load", function(){
-          setTimeout(function(){
-            //reset to hide addr bar at onload
-            win.scrollTo( 0, scrollTop === 1 ? 0 : 1 );
-          }, 0);
-        }, false );
-      }
-    }
-  
-  })();
-
   return (
     <div className="App" style={{ height: "100vh" }}>
-      <MainContainer />
+      <FullScreen handle={handle}>
+        <MainContainer />
+      </FullScreen>
+      <Button onClick={handle.enter}>Full screen</Button>
     </div>
   );
 }
