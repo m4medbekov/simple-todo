af7f1661cf7d60262a9ba830a6385d05bd811904
Ruslan Mamedbekov <m4medbekov@gmail.com>
2022-08-16 12:34:53 +0300

    back

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import './App.css';
 import 'antd/dist/antd.min.css';
 import MainContainer from './components/MainContainer';
-import { Button } from 'antd';
-import { FullScreen, useFullScreenHandle } from "react-full-screen";
-
 
 function App() {
-
-  const handle = useFullScreenHandle();
-  
   return (
     <div className="App" style={{ height: "100vh" }}>
-      <FullScreen handle={handle}>
-        <MainContainer />
-      </FullScreen>
-      <Button onClick={handle.enter}>Full screen</Button>
+      <MainContainer />
     </div>
   );
 }
